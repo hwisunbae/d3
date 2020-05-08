b9acafccc186c87d1b2f65c054f0826dd00d96f9
Hwisun Bae <hwisunbae.hb@gmail.com>
2020-05-08 17:27:31 +0900

    continents.json uplaoded

---
+++ src/js/main2.js
@@ -0,0 +1,19 @@
+const margin = {
+  right: 20,
+  left: 20,
+  bottom :10,
+  top :30
+}
+
+const width = 500 - margin.right - margin.left,
+  height = 500 - margin.top - margin.bottom;
+
+let svg = d3.select("#chart-area")
+.append('svg')
+  .attr('width', width + margin.right + margin.left)
+  .attr('height', height + margin.top + margin.bottom);
+
+d3.json('data/continents.json').then(data => {
+  window.data = data;
+}).catch(err => console.log(err))
+
